c5759c50f50a0e29ec07870924eb04c518c48e66
Lorenzo Pichilli <pichillilorenzo@gmail.com>
2024-10-02 16:04:17 +0200

    updated .github/workflows/main.yml, removed share_extend dependency

--- lib/app_bar/webview_tab_app_bar.dart
+++ lib/app_bar/webview_tab_app_bar.dart
@@ -19,7 +19,6 @@ import 'package:flutter_font_icons/flutter_font_icons.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
-import 'package:share_extend/share_extend.dart';
 import 'package:share_plus/share_plus.dart';
 
 import '../animated_flutter_browser_logo.dart';
@@ -1308,7 +1307,7 @@ class _WebViewTabAppBarState extends State<WebViewTabAppBar>
               ElevatedButton(
                 child: const Text("Share"),
                 onPressed: () async {
-                  await ShareExtend.share(file.path, "image");
+                  await Share.shareXFiles([XFile(file.path)]);
                 },
               )
             ],
